c79050899b64743b325a6d7308687ba60dd7ecc4
Konrad Borowski <glitchmr@myopera.com>
2013-04-26 21:07:09 +0300

    It's CLI, not CGI

--- off-rails.php
+++ off-rails.php
@@ -36,7 +36,7 @@ error_reporting(-1);
 error_reporting(-1);
 
 if (!isset($_SERVER['REQUEST_METHOD']))
-    die("Stop wasting my time running me on CGI!\n");
+    die("Stop wasting my time running me on CLI!\n");
 
 if (get_magic_quotes_gpc()) {
     // PHP doesn't have lexical variables. I've to hack them!
